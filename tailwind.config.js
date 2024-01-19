b7f0c83b48ff7b368d0a17abf23c5267e6462fb2
Jose Lima <jalimag0206@gmail.com>
2024-01-19 02:47:35 -0300

    feat: add header component

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,6 +2,27 @@
 export default {
     content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
     theme: {
+        boxShadow: {
+            inputShadow: '1px 1px 3px 0px rgba(0, 0, 0, 0.25)',
+            headerShadow: '1px 1px 10px 1px rgba(0, 0, 0, 0.25)',
+        },
+        colors: {
+            gray400: '#455A64',
+            gray100: '#D9D9D9',
+            gray200: '#9E9E9E',
+            blue: '#BAE2FF',
+            lightGreen: '#B9FFDD',
+            amber: '#FFE8AC',
+            orange200: '#FFCAB9',
+            red: '#F99494',
+            lightBlue: '#9DD6FF',
+            purple: '#ECA1FF',
+            lime: '#DAFF8B',
+            orange100: '#FFA285',
+            grey400: '#CDCDCD',
+            grey600: '#979797',
+            yellow: '#A99A7C',
+        },
         extend: {},
     },
     plugins: [],
