e3150622759d9d1baf5266903b72d30170fd763d
Jose Lima <jalimag0206@gmail.com>
2024-01-20 02:10:26 -0300

    feat: add TaskContext

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,17 @@
 import { Header } from './components/Header'
 import { TaskInput } from './components/TaskInput'
 import { TaskList } from './components/TaskList'
+import { TaskProvider } from './context/TaskContext'
 
 function App() {
     return (
-        <>
+        <TaskProvider>
             <Header />
             <TaskInput />
             <div className="md:container mx-auto">
                 <TaskList />
             </div>
-        </>
+        </TaskProvider>
     )
 }
 
---
+++ src/context/TaskContext.tsx
@@ -0,0 +1,65 @@
+import { ReactNode, createContext, useEffect, useState } from 'react'
+import { api } from '../services/api'
+
+export interface ITaskProps {
+    id: string
+    title: string
+    description: string
+    color: string
+    isFavorited: boolean
+}
+
+interface ITaskContextType {
+    tasks: ITaskProps[]
+    editTask: (
+        taskId: string,
+        updatedTaskData: Partial<ITaskProps>
+    ) => Promise<void>
+}
+
+const TaskContext = createContext({} as ITaskContextType)
+
+interface TaskProviderProps {
+    children: ReactNode
+}
+
+export function TaskProvider({ children }: TaskProviderProps) {
+    const [tasks, setTasks] = useState<ITaskProps[]>([])
+
+    async function getTask() {
+        const response = await api.get('/tasks')
+        const data = response.data
+
+        setTasks(data.tasks)
+    }
+
+    async function editTask(
+        taskId: string,
+        updatedTaskData: Partial<ITaskProps>
+    ) {
+        const response = await api.patch(`/tasks/${taskId}`, updatedTaskData)
+
+        const updatedTask = response.data
+
+        setTasks((prevTasks) => {
+            return prevTasks.map((task) => {
+                if (task.id === updatedTask.id) {
+                    return { ...task, ...updatedTask }
+                }
+                return task
+            })
+        })
+    }
+
+    useEffect(() => {
+        getTask()
+    }, [])
+
+    return (
+        <TaskContext.Provider value={{ tasks, editTask }}>
+            {children}
+        </TaskContext.Provider>
+    )
+}
+
+export default TaskContext
